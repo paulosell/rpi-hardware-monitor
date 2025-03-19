6a2df34aeefe1be9b38bc4c7ce59ac5107939b86
Paulo Sell <sellpaulof@gmail.com>
2025-03-19 16:47:54 -0300

    Update MAX_TEMP and sysfs file definitions for RPI5

--- include/cpu_thermal_monitor.h
+++ include/cpu_thermal_monitor.h
@@ -7,8 +7,8 @@
 #include <syslog.h>
 #include <errno.h>
 
-#define SYSFS_THERMAL_FILE "/sys/class/hwmon/hwmon3/temp2_input"
-#define RPI_CPU_MAX_TEMP 85.0 
+#define SYSFS_THERMAL_FILE "/sys/class/thermal/thermal_zone0/temp"
+#define RPI_CPU_MAX_TEMP 75.0
 #define TEMPERATURE_MULTIPLIER 0.001
 
 enum {
